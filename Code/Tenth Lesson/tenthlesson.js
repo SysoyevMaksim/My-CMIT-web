098aa820f02161bbc38975d2e28238cdab986c5e
Maksim <sysoyev.maxim@gmail.com>
2020-07-13 10:25:53 +0300

    Calculator v1.1 (+point and noletters)

--- Code/Tenth Lesson/tenthlesson.js
+++ Code/Tenth Lesson/tenthlesson.js
@@ -1,27 +1,33 @@
 let count = 0;
 let now;
+let double = -1;
 
 let is_plus = false;
 let is_minus = false;
 let is_multiply = false;
 let is_divide = false;
 let calculated = false;
+let is_point = false;
 
 function add_number(number) {
     const result = document.getElementById("result");
-    if (result.value === ""){
-        result.value = "0";
+    if (result.value === "0"){
+        result.value = "";
     }
-    const result_value = Number.parseInt(result.value);
-    result.value = result_value * 10 + Number.parseInt(number);
-    now = result_value * 10 + Number.parseInt(number);
+    const result_value = result.value.toString();
+    if (!is_point) {
+        result.value = result_value + number;
+    } else {
+        result.value = result_value.substring(0, result_value.length-1) + number;
+    }
+    now = Number.parseFloat(result.value);
 }
 
 function delete_number() {
     const result = document.getElementById("result");
-    const result_value = Number.parseInt(result.value);
-    result.value = Math.floor(result_value / 10);
-    now = Math.floor(result_value / 10);
+    const result_value = result.value.toString();
+    result.value = result_value.substring(0, result_value.length-1);
+    now = Number.parseFloat(result.value);
 }
 
 function calculate() {
@@ -114,4 +120,13 @@ function multiply() {
 function zero() {
     const result = document.getElementById("result");
     result.value = 0;
+}
+
+function point() {
+    const result = document.getElementById("result");
+   // console.log("Hello");
+    const result_value = result.value.toString();
+    result.value = result_value+".0";
+    is_point = true;
+    //now = Number.parseFloat(result.value);
 }
